580af517eb1067a3957633fab9f04642b609e343
IT26101565 <it26101565@my.sliit.lk>
2026-09-15 21:59:30 +0530

    IT26101565Lab7Q3.java

---
+++ Q3/IT26101565Lab7Q3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IT26101565Lab7Q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Customer " + i);
+            System.out.print("Enter total bill amount: ");
+            double billAmount = scanner.nextDouble();
+            
+            System.out.print("Enter mode of payment (C for cash, O for other): ");
+            char mode = scanner.next().charAt(0);
+            
+            if (mode == 'C' || mode == 'c') {
+                double discount = billAmount * 0.05;
+                double amountPaid = billAmount - discount;
+                System.out.println("Discount is : " + discount);
+                System.out.println("Amount to be paid : " + amountPaid);
+            } else if (mode == 'O' || mode == 'o') {
+                System.out.println("No discount applicable");
+                System.out.println("Amount to be paid : " + billAmount);
+            } else {
+                System.out.println("Payment Mode is Not valid");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
